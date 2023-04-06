235b406e7258132b9a2b3f43bcc3787b6524bde2
dhtjddls <dhtjddls12@naver.com>
2023-04-06 13:52:42 +0900

    feat: 노드 기본 설정

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const port = 8000;
 const app = express();
+const mongoose = require("mongoose");
+
+app.set("view engine", "ejs");
+app.use("/views", express.static(__dirname + "/views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+mongoose.connect(
+  "mongodb+srv://sparta:test@cluster0.xzw0iic.mongodb.net/?retryWrites=true&w=majority"
+);
 
 app.get("/", (req, res) => {
-  res.send("hi!");
+  res.render("index");
 });
 
 app.listen(port, () => console.log(`running on http://localhost:${port}`));
---
+++ models/Func.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+// id, 이름, 설명, 점수,
+const Func = new Schema({
+  id: Number,
+  name: String,
+  content: String,
+  score: String,
+});
+
+const FuncModel = model("Func", Func);
+
+module.exports = FuncModel;
